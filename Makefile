353e2e4cd95e985de82a26921e12ff89ea0a71ce
Jean-Marie Renouard <jmrenouard@gmail.com>
2022-12-29 19:03:07 +0100

    Last updates

--- Makefile
+++ Makefile
@@ -1,19 +1,40 @@
 
 all: godb
 
-exec: godb
+all: test vet fmt lint build run
+
+run: godb
 	./godb
+
+build:
+    go build -o bin/api ./cmd/api
+    go build -o bin/worker ./cmd/worker
 
 godb: *.go
 	go build -ldflags="-s -w" .
+
 test:
 	go test -coverprofile=cover.out -v
+
 coverage: test
 	go tool cover -func=cover.out
+
 coverage-html:
 	go tool cover -html=cover.out
+
+fmt:
+    go list -f '{{.Dir}}' ./... | grep -v /vendor/ | xargs -L1 gofmt -l
+    test -z $$(go list -f '{{.Dir}}' ./... | grep -v /vendor/ | xargs -L1 gofmt -l)
+
+lint:
+    go list ./... | grep -v /vendor/ | xargs -L1 golint -set_exit_status
+
+vet:
+    go vet ./...
+
 install:
 	go install .
+
 clean:
 	rm -rf test
 	rm cover.out godb
--- goutils/fsutils.go
+++ goutils/fsutils.go
@@ -11,3 +11,38 @@ func TouchFile(name string) error {
 	}
 	return file.Close()
 }
+
+func ReadFileContent(name string) ([]byte, error) {
+	file, err := os.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	fileinfo, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	filesize := fileinfo.Size()
+	buffer := make([]byte, filesize)
+
+	bytesread, err := file.Read(buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	if int64(bytesread) != filesize {
+		return nil, errors.New("ReadFileContent: could not read whole file")
+	}
+
+	return buffer, nil
+}
+
+func ReadTextFileContent(name string) (string, error) {
+	buffer, err := ReadFileContent(name)
+	if err != nil {
+		return "", err
+	}
+	return string(buffer), nil
+}
